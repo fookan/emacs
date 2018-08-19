6b887e0924497c6d7f4f101d844a0eb864632bd5
fookan <die4love.mk@gmail.com>
2018-08-19 21:47:09 +0900

    emacsの設定ファイル

---
+++ .emacs.d/elpa/dired-subtree-20170910.1521/dired-subtree-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-subtree" "20170910.1521" "Insert subdirectories in a tree-like fashion" '((dash "2.5.0") (dired-hacks-utils "0.0.1")) :commit "3fd347a0823312e966872bd0d26d9a75b2898c9c" :keywords '("files"))
