3863b115fe76a8bb27514f110b37fe4d8a35b116
die4lovemk <die4love.mk@live.com>
2020-02-22 04:57:51 +0900

    add theme

---
+++ elpa/cdb-20200210.1326/cdb-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cdb" "20200210.1326" "constant database (cdb) reader for Emacs Lisp" 'nil :commit "8b6986470917a9e66fb9aa76fe5c7f79aee62293" :keywords '("cdb") :authors '(("Yusuke Shinyama <yusuke at cs . nyu . edu>")) :maintainer '("SKK Development Team" . "skk@ring.gr.jp"))
---
+++ elpa/undohist-20200120.1328/undohist-autoloads.el
@@ -0,0 +1,15 @@
+;;; undohist-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("undohist.el") (24135 60451 814990 128000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; undohist-autoloads.el ends here
