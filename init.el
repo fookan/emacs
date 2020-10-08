197db404914c87b649c1bae4e59c76dab784b68c
fookan <>
2020-10-09 04:39:28 +0900

    add company

--- init.el
+++ init.el
@@ -11,12 +11,21 @@
 ;; flycheck
 ;; add-node-modules-path
 ;; fuzzy
+;; company
+;; proectile
+
+;; projectile
+; gitのfolder構成に管理できるのでgit initしてぷろじぇくとをつくる 
 
 ;;; package
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+;;; mozc
+(require 'mozc) ; mozcの読み込み
+(setq default-input-method "japanese-mozc") ; IMEをjapanes-mozcに
 
 ;;; 環境を日本語、UTF-8にする
 (set-language-environment "Japanese")
@@ -206,12 +215,12 @@
 ;; font
 ; フォントをインストール
 ; fc-list でフォントを確認
-(set-fontset-font nil 'unicode "TakaoGothic")
-(set-face-attribute 'default nil :height 130)
+; (set-fontset-font nil 'unicode "TakaoGothic")
+; (set-face-attribute 'default nil :height 130)
 
 ;; window
-(set-frame-size (selected-frame) 130 40)
-(set-frame-position (selected-frame) 0 0)
+; (set-frame-size (selected-frame) 130 40)
+; (set-frame-position (selected-frame) 0 0)
 
 ;; flycheck
 (when (require 'flycheck nil t)
@@ -245,6 +254,66 @@
 ;      'mew-send-hook))
 
 
+;; company
+(when (require 'company nil t)
+  (setq company-auto-expand t) ;; 1個目を自動的に補完
+  (setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
+  (setq company-idle-delay 0) ; 遅延なしにすぐ表示
+  (setq company-minimum-prefix-length 2) ; デフォルトは4
+  (setq company-selection-wrap-around t) ; 候補の最後の次は先頭に戻る
+  (setq completion-ignore-case t)
+  (setq company-dabbrev-downcase nil)
+  (global-set-key (kbd "C-M-i") 'company-complete)
+  ;; C-n, C-pで補完候補を次/前の候補を選択
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+
+  (define-key company-active-map (kbd "C-h") nil) ;; C-hはバックスペース割当のため無効化
+  (define-key company-active-map (kbd "C-S-h") 'company-show-doc-buffer) ;; ドキュメント表示はC-Shift-h
+  
+  ;; 未選択項目
+  (set-face-attribute 'company-tooltip nil
+                      :foreground "#36c6b0" :background "#244f36")
+  ;; 未選択項目&一致文字
+  (set-face-attribute 'company-tooltip-common nil
+                      :foreground "white" :background "#244f36")
+  ;; 選択項目
+  (set-face-attribute 'company-tooltip-selection nil
+                      :foreground "#a1ffcd" :background "#007771")
+  ;; 選択項目&一致文字
+  (set-face-attribute 'company-tooltip-common-selection nil
+                      :foreground "white" :background "#007771")
+  ;; スクロールバー
+  (set-face-attribute 'company-scrollbar-fg nil
+                      :background "#4cd0c1")
+  ;; スクロールバー背景
+  (set-face-attribute 'company-scrollbar-bg nil
+                      :background "#002b37")
+  ; tab
+  ; (define-key company-active-map [tab] 'company-complete-selection) ;; TABで候補を設定
+  ; 候補が１つの場合はそれを選択する．
+ ; 候補が複数の場合，挿入可能なprefixがあれば挿入し，なければcompany-select-nextする
+  ;; (defun company--insert-candidate2 (candidate)
+  ;;   (when (> (length candidate) 0)
+  ;;     (setq candidate (substring-no-properties candidate))
+  ;;     (if (eq (company-call-backend 'ignore-case) 'keep-prefix)
+  ;;         (insert (company-strip-prefix candidate))
+  ;;       (if (equal company-prefix candidate)
+  ;;           (company-select-next)
+  ;;         (delete-region (- (point) (length company-prefix)) (point))
+  ;;         (insert candidate))
+  ;;       )))
+
+  ;; (defun company-complete-common2 ()
+  ;;   (interactive)
+  ;;   (when (company-manual-begin)
+  ;;     (if (and (not (cdr company-candidates))
+  ;;              (equal company-common (car company-candidates)))
+  ;;         (company-complete-selection)
+  ;;       (company--insert-candidate2 company-common))))
+
+  ;; (define-key company-active-map [tab] 'company-complete-common2)
+  )
 
 ;;; init.el
 (custom-set-variables
@@ -254,7 +323,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (mew fuzzy add-node-modules-path flycheck undo-tree undohist color-theme-sanityinc-tomorrow auto-complete))))
+    (mozc projectile company ## mew fuzzy add-node-modules-path flycheck undo-tree undohist color-theme-sanityinc-tomorrow auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
